e556787753affcbc7099928dd5d3e882feb7e3b5
Tyrell Williams <tyrellwilliams2020@gmail.com>
2020-06-26 15:26:48 -0400

    updated morse.py

--- morse.py
+++ morse.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import re
 """
 Morse code decoder
 
@@ -50,17 +51,42 @@ MORSE_CODE = {
 
 def find_mult(bits):
     """Finds least amount of occurrences of 0 or 1"""
-    # your code here
-    return
+    smallest_zeros = len(min(
+        [group for group in bits.split('1') if group != '']
+        ))
+    smallest_ones = len(min(
+        [group for group in bits.split('0') if group != '']
+    ))
+    return min (smallest_zeros, smallest_ones)
 
 
-def decodeBits(bits):
+def decode_bits(bits):
     """Translate a string of 1's & 0's to dots and dashes"""
-    # your code here
-    return
+    bits = bits.strip('0')
+    if '0' not in bits:
+        time_unit = len(bits)
+    else:
+        time_unit = find_mult(bits)
+    seven_units = str(time_unit * 7)
+    three_units = str(time_unit * 3)
+    one_unit = str(time_unit)
+    sentence = re.sub(r'0{' + seven_units + r'}', '  ', bits)
+    sentence = re.sub(r'0{' + three_units + r'}', '  ', sentence)
+    morse_code = re.sub(r'1{' + three_units + r'}', '-', sentence)
+    morse_code = re.sub(r'1{' + one_unit + r'}', '.', morse_code)
+    morse_code = re.sub(r'0{' + one_unit + r'}', '', morse_code)
+    return morse_code 
 
 
-def decodeMorse(morse_code):
+
+def decode_morse(morse_code):
     """Translates a string of dots and dashes to human readable text"""
-    # your code here
-    return
+    morse_words = morse_code.split('  ')
+    decoded_words = []
+    for morse_word in morse_words:
+        for morse_char in morse_word.split():
+            decoded_words.append(MORSE_CODE[morse_char])
+        decoded_words.append(' ')
+    return ''.join(decoded_words).strip()
+
+print(decode_morse(decode_bits('1100110011001100000011000000111111001100111111001111110000000000000011001111110011111100111111000000110011001111110000001111110011001100000011')))
